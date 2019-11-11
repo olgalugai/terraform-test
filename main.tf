557b4e9782ac020332fc4346dde775e2f47ff87b
Olga Lugai <olgalugai@Olgas-MacBook-Pro.local>
2019-11-11 15:28:54 +0000

    Initial

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=1.36.0"
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "TestResourceGroup1"
+  location = "uksouth"
+}
+
+resource "azurerm_virtual_network" "test" {
+  name                = "acceptanceTestVirtualNetwork1"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+
+resource "azurerm_subnet" "test" {
+  name                 = "testsubnet"
+  resource_group_name  = "${azurerm_resource_group.test.name}"
+  virtual_network_name = "${azurerm_virtual_network.test.name}"
+  address_prefix       = "10.0.1.0/24"
+}
