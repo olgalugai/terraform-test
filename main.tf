bd8a241b73d87b566cf321fbc5b5c61dc8b6d1a8
Olga Lugai <olgalugai@Olgas-MacBook-Pro.local>
2019-11-12 12:00:39 +0000

    Fix

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=1.36.0"
-}
-
 resource "azurerm_resource_group" "test" {
   name     = "TestResourceGroup1"
   location = "uksouth"
