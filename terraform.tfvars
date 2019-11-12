e658345a21a88bd76e2ec1ceb3e5fcb7aa15263d
Olga Lugai <olgalugai@Olgas-MacBook-Pro.local>
2019-11-12 13:40:25 +0000

    Fix

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
-subscription_id   = "088de0df-dacc-46b9-8e67-f92a2004e935"
+/*subscription_id   = "088de0df-dacc-46b9-8e67-f92a2004e935"
 client_id 		  = "289161d9-7e5e-418a-ad5a-3ff7e68daa16"
 client_secret 	  = "b19ce7b7-5649-4d21-9105-7156f9b1dfe0"
-tenant_id         = "152e4c75-3657-4284-bb2c-0d825873d72b"
+tenant_id         = "152e4c75-3657-4284-bb2c-0d825873d72b"*/
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-provider "azurerm" {
+/*provider "azurerm" {
   subscription_id        = "${var.subscription_id}"
   client_id              = "${var.client_id}"
   client_secret          = "${var.client_secret}"
@@ -9,4 +9,4 @@ provider "azurerm" {
 variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
-variable "tenant_id" {}
+variable "tenant_id" {}*/
