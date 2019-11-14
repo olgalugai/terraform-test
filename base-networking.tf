5fab030d8c3a6848301fc98cfc19aee97949eaac
Olga Lugai <olgalugai@Olgas-MacBook-Pro.local>
2019-11-14 15:14:45 +0000

    Fix

--- base-networking.tf
+++ base-networking.tf
@@ -1,12 +1,13 @@
 module vnet {
-    source "/../../modules/CoreResources/base-networking"
+    source "./modules/base-networking"
     vnet_name   = "${var.vnet_name}"
     subnet_name = "${var.subnet_name}"
     subnet_cidr = "${var.subnet_cidr}"
     rg_name =     "${var.rg_name}"
 }
 
-provider "azurerm" {
+module provider {
+     source: "./modules/provider"
   subscription_id        = "${var.subscription_id}"
   client_id              = "${var.client_id}"
   client_secret          = "${var.client_secret}"
