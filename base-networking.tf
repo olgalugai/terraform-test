16cd5a5b11fdc82d585c8dec7f815c8056dd8e23
Olga Lugai <olgalugai@Olgas-MacBook-Pro.local>
2019-11-14 15:36:30 +0000

    Fix

--- base-networking.tf
+++ base-networking.tf
@@ -7,10 +7,3 @@ module vnet {
     rg_name     = "${var.rg_name}"
 }
 
-module provider {
-  source                 = "./modules/provider"
-  subscription_id        = "${var.subscription_id}"
-  client_id              = "${var.client_id}"
-  client_secret          = "${var.client_secret}"
-  tenant_id              = "${var.tenant_id}"
-}
--- provider.tf
+++ provider.tf
@@ -6,3 +6,8 @@ provider "azurerm" {
 }
 
 
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+
