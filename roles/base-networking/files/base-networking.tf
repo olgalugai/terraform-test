0891ff228a2bf75422a48942928e75ba3167c16f
Olga Lugai <olgalugai@Olgas-MacBook-Pro.local>
2019-11-14 14:24:26 +0000

    Fix

--- roles/base-networking/files/base-networking.tf
+++ roles/base-networking/files/base-networking.tf
@@ -1,9 +1,9 @@
 module vnet {
     source "../../../../../modules/CoreResources/base-networking"
-vnet_name = "${var.vnet_name}"
-subnet_name = "${var.subnet_name}"
-subnet_cidr = "${var.subnet_cidr}"
-rg_name = "${var.rg_name}"
+    vnet_name   = "${var.vnet_name}"
+    subnet_name = "${var.subnet_name}"
+    subnet_cidr = "${var.subnet_cidr}"
+    rg_name =     "${var.rg_name}"
 }
 
 provider "azurerm" {
