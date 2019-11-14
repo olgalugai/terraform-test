0c2b9103ca705e124c9a4ce154aa2659d6d1a1cd
Olga Lugai <olgalugai@Olgas-MacBook-Pro.local>
2019-11-14 15:30:31 +0000

    Fix

--- modules/base-networking/main.tf
+++ modules/base-networking/main.tf
@@ -14,5 +14,5 @@ resource "azurerm_subnet" "test" {
   name                 = "${var.subnet_name}"
   resource_group_name  = "${azurerm_resource_group.test.name}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
-  address_prefix       = ["${var.subnet_cidr}"]
+  address_prefix       = "${var.subnet_cidr}"
 }
